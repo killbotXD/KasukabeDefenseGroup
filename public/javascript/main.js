5d24fc46b8aa4d1b100ad91d06071839d71dab46
ssahillppatell <ssahillppatell@gmail.com>
2019-08-15 20:54:16 +0530

    Updated favicon and radio button input

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,16 @@
 $(document).ready(() => {
     const form = $("#form");
-    const display = $("#display");
+    const form2 = $("#form2");
     const input = $("#input");
+    const sec1 = $("#sec1");
+    const sec2 = $("#sec2");
 
     const buildTemplate = (doc) => {
-        return `<input type = "radio">${doc._candidates[0].__name}`;
+        return `<input type = "radio" name = "candidate" value = "0">${doc._candidates[0].__name} | ${doc._candidates[0].__partyName}<br>
+        <input type = "radio" name = "candidate" value = "1">${doc._candidates[1].__name} | ${doc._candidates[1].__partyName}<br>
+        <input type = "radio" name = "candidate" value = "2">${doc._candidates[2].__name} | ${doc._candidates[2].__partyName}<br>
+        <input type = "radio" name = "candidate" value = "3">${doc._candidates[3].__name} | ${doc._candidates[3].__partyName}<br>
+        <button type = "submit">Submit</button>`;
     }
 
     form.submit((e) => {
@@ -17,9 +23,9 @@ $(document).ready(() => {
         }).then((response) => {
             return response.json();
         }).then((data) => {
-            display.append(buildTemplate(data));
+            form2.append(buildTemplate(data));
         }).then(() => {
-            form.hide();
+            sec1.hide();
         });
     });
 });
