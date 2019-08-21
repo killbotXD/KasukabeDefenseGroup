def6fbeefd5aad78a933ba52ae7ab669eaf7e3e0
ssahillppatell <ssahillppatell@gmail.com>
2019-08-21 23:07:46 +0530

    Completed stats page

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, './app/index.html'));
 });
 
+app.get('/stats', function(req, res){
+	res.sendFile(path.join(__dirname, './app/stats.html'));
+});
+
 app.get('/:id', (req, res) => {
     const vId = req.params.id;
     db.getDb().collection(collection).findOne({ _voterId : vId }, (err, doc) => {
@@ -27,6 +31,38 @@ app.get('/:id', (req, res) => {
         else {
 			console.log(doc);
 			res.jsonp(doc);
+		}
+    });
+});
+
+
+app.get('/stats/:city', (req, res) => {
+	let slot0 = 0, slot1 = 0, slot2 = 0, slot3 = 0;
+	const city = req.params.city;
+	db.getDb().collection(collection).find({}).toArray(function(err, doc){
+        if(err)
+            console.log("Error");
+        else {
+			doc.forEach(data => {
+				switch(data._choice)
+				{
+					case 0:
+						slot0++;
+						break;
+					case 1:
+						slot1++;
+						break;
+					case 2:
+						slot2++;
+						break;
+					case 3:
+						slot3++;
+						break;
+				}	
+			});
+			let obj = { _slot0 : slot0, _slot1 : slot1, _slot2 : slot2, _slot3 : slot3};
+			console.log(JSON.stringify(obj));
+			res.jsonp(obj);
 		}
     });
 });
